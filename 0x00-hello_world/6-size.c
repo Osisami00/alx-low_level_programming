d52661b633080dde82cf4717321436d1c6ff1301
osisami00 <osisamimichael1@gmail.com>
2022-06-30 16:21:53 -0700

    Continuation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <stdio.h>
+
+/**
+ * main - print the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	printf("size of a char: %d byte(s)\n", sizeof (char));
+	printf("size of an int: %d byte(s)\n", sizeof (int));
+	printf("size of long int a char: %d byte(s)\n", sizeof (char));
